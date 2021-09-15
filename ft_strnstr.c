b978f079baeb25dee30cdad602f1b070f227074b
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-15 15:39:35 -0300

    Fixed while statement

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,7 +11,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	{
 		little_len = ft_strlen(little);
 		i = 0;
-		while (i < len && i < little_len)
+		while ((i < len && i < little_len) || i < little_len)
 		{
 
 			if (ft_strncmp((big + i), (little), little_len) == 0)
