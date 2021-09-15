3b2fc6f1717fa5cffd5801c771a57348ef5d1d47
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-15 14:10:39 -0300

    iterator moved out of condition

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -5,12 +5,12 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	little_len;
 	size_t	i;
 
+	i = 0;
 	if (len > 0)
 	{
 		if (*little == '\0')
 			return ((char *) big);
 		little_len = ft_strlen(little);
-		i = 0;
 		while (i < len - little_len)
 		{
 			if (ft_strncmp((big + i), (little), little_len) == 0)
