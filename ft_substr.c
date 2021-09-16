3a2d63305c289bff46b1e5abe7fc5d8da0cc1994
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-16 09:39:14 -0300

    Testing new algorithm

--- ft_substr.c
+++ ft_substr.c
@@ -4,6 +4,25 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*subs;
 	size_t	s_len;
+	size_t	subs_len;
+
+	s_len = ft_strlen(s);
+	subs_len = len;
+	if (start >= s_len)
+		return (ft_strdup(""));
+	else if (start + len >= s_len)
+		subs_len = s_len - start;
+	subs = ft_calloc(subs_len + 1, sizeof(char));
+	if (!subs)
+		return ((void *) 0);
+	ft_strlcpy(subs, (s + start), subs_len);
+	return (subs);
+}
+
+/*char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	s_len;
 
 	s_len = ft_strlen(s);
 	if (len > s_len)
@@ -13,4 +32,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return ((void *) 0);
 	ft_strlcpy(subs, (s + start), len + 1);
 	return (subs);
-}
+}*/
