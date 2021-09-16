b910b74ef656e355b78b8c202b6c3b22015213a6
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-16 09:40:37 -0300

    Removed comments

--- ft_substr.c
+++ ft_substr.c
@@ -18,18 +18,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	ft_strlcpy(subs, (s + start), subs_len + 1);
 	return (subs);
 }
-
-/*char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*subs;
-	size_t	s_len;
-
-	s_len = ft_strlen(s);
-	if (len > s_len)
-		len = s_len;
-	subs = ft_calloc(len + 1, sizeof(char));
-	if (!subs)
-		return ((void *) 0);
-	ft_strlcpy(subs, (s + start), len + 1);
-	return (subs);
-}*/
