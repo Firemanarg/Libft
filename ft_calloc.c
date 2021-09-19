b911be357d327eda70c2f43c5be5e2bf083eb19b
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-19 11:31:28 -0300

    Added bzero call, to set allocated bytes to zero

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:14:54 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/19 11:14:55 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/19 11:30:42 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	return (malloc(nmemb * size));
+	void	*out;
+
+	out = malloc(nmemb * size);
+	if (out)
+		ft_bzero(out, nmemb * size);
+	return (out);
 }
