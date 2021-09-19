57bd800c0ae9159c4e0fce6327e400f0d1992ba6
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-19 12:12:28 -0300

    Returned to previous logic

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:20 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/19 12:08:38 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/19 12:12:08 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,5 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	len_s1;
-	size_t	len_s2;
-	int		result;
-
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	if (len_s1 < len_s2)
-		result = ft_memcmp(s1, s2, len_s1);
-	else
-		result = ft_memcmp(s1, s2, len_s2);
-	return (result);
+	return (ft_memcmp(s1, s2, n - 1));
 }
