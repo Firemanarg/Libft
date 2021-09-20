a48b72e123f96c7f95034cc8afae4e2c6991f283
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-19 22:00:13 -0300

    Added malloc protection

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:47 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/19 11:13:48 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/19 22:00:00 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strdup(const char *s)
 
 	len = ft_strlen(s);
 	new_str = ft_calloc(len + 1, sizeof(char));
+	if (!new_str)
+		return (NULL);
 	ft_strlcpy(new_str, s, len + 1);
 	return (new_str);
 }
