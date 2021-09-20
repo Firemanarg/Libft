dabd5a8a0c0237fd27345bf966e3d7610e84f86b
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-20 09:04:18 -0300

    Protected function

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:28 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/19 11:13:30 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/20 09:04:04 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *str)
 {
 	size_t	len;
 
+	if (!str)
+		return (0);
 	len = 0;
 	while (*(str + len) != '\0')
 		len += 1;
