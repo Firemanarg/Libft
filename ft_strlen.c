41b23e185a59c54454ab2409099e1f1e883f6493
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-20 09:05:23 -0300

    Unprotected function

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:28 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/20 09:04:04 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/20 09:05:14 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ size_t	ft_strlen(const char *str)
 {
 	size_t	len;
 
-	if (!str)
-		return (0);
 	len = 0;
 	while (*(str + len) != '\0')
 		len += 1;
