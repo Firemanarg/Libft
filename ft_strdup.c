4e313a22fdf24594b1f9e60cc8d43ef86b527ef5
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-20 08:59:09 -0300

    Protected function

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:47 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/19 22:00:00 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/20 08:58:53 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ char	*ft_strdup(const char *s)
 	char	*new_str;
 	size_t	len;
 
-	len = ft_strlen(s);
-	new_str = ft_calloc(len + 1, sizeof(char));
-	if (!new_str)
-		return (NULL);
-	ft_strlcpy(new_str, s, len + 1);
-	return (new_str);
+	if (!s)
+	{
+		len = ft_strlen(s);
+		new_str = ft_calloc(len + 1, sizeof(char));
+		if (!new_str)
+			return (NULL);
+		ft_strlcpy(new_str, s, len + 1);
+		return (new_str);
+	}
+	return (NULL);
 }
