407c0e97f2887d7205af51d9f119a4137b628317
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-19 21:42:31 -0300

    Changed logic

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:20 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/19 12:12:58 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/19 21:42:09 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	return (ft_memcmp(s1, s2, n));
+	size_t			i;
+	unsigned char	*uns_chr1;
+	unsigned char	*uns_chr2;
+
+	uns_chr1 = (unsigned char *) s1;
+	uns_chr2 = (unsigned char *) s2;
+	i = 0;
+	while (s1 && s2 && i < n)
+		i += 1;
+	return (*(uns_chr1 + i) - *(uns_chr2 + i));
 }
