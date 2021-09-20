9a8c063a682805ee366f641422f58cbd296ccf71
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-20 09:04:36 -0300

    Testing new logic

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:47 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/20 09:00:57 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/20 09:03:40 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,10 @@ char	*ft_strdup(const char *s)
 	char	*new_str;
 	size_t	len;
 
-	if (!s)
-	{
-		len = ft_strlen(s);
-		new_str = ft_calloc(len + 1, sizeof(char));
-		if (!new_str)
-			return (NULL);
-		ft_strlcpy(new_str, s, len + 1);
-		return (new_str);
-	}
-	return (ft_calloc(1, sizeof(char)));
+	len = ft_strlen(s);
+	new_str = ft_calloc(len + 1, sizeof(char));
+	if (!new_str)
+		return (NULL);
+	ft_strlcpy(new_str, s, len + 1);
+	return (new_str);
 }
