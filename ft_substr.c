a9751fbe9df57b068ee1b02fce1c096a52e16969
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-20 08:41:25 -0300

    Protected function

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:04 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/19 11:36:36 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/20 08:41:14 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	s_len;
 	size_t	subs_len;
 
-	s_len = ft_strlen(s);
-	subs_len = len;
-	if (start >= s_len)
-		return (ft_strdup(""));
-	else if (start + len >= s_len)
-		subs_len = s_len - start;
-	subs = ft_calloc(subs_len + 1, sizeof(char));
-	if (!subs)
-		return (NULL);
-	ft_strlcpy(subs, (s + start), subs_len + 1);
-	return (subs);
+	if (s)
+	{
+		s_len = ft_strlen(s);
+		subs_len = len;
+		if (start >= s_len)
+			return (ft_strdup(""));
+		else if (start + len >= s_len)
+			subs_len = s_len - start;
+		subs = ft_calloc(subs_len + 1, sizeof(char));
+		if (!subs)
+			return (NULL);
+		ft_strlcpy(subs, (s + start), subs_len + 1);
+		return (subs);
+	}
+	return (NULL);
 }
