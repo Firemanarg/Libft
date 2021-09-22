eaa4bf00b98f18163699236026e8748f679de9c0
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-22 14:11:56 -0300

    Implemented new logic

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,12 +6,39 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:59 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/20 08:51:38 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/22 14:11:11 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	ft_putnbr_rec(unsigned int n, int fd)
+{
+	unsigned int	ncpy;
+
+	ncpy = n;
+	while (n / 10 != 0)
+		n /= 10;
+	write(fd, &('0' + n), 1);
+	if (n != 0)
+		ft_putnbr_rec(ncpy / 10, fd);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nbr;
+
+	if (n < 0)
+	{
+		nbr = n * -1;
+		write(fd, "-", 1);
+	}
+	else
+		nbr = n;
+	ft_putnbr_rec(nbr, fd);
+}
+
+/*
 void	ft_putnbr_fd(int n, int fd)
 {
 	char	*nbr;
@@ -23,3 +50,4 @@ void	ft_putnbr_fd(int n, int fd)
 		free(nbr);
 	}
 }
+*/
