04f4717b3f6ddcad414451286bd2eac6009a850b
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-22 15:13:13 -0300

    Removed old script

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,13 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:59 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/22 14:49:43 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/22 15:12:11 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_putnbr_rec(unsigned int n, int fd, int mult)
+static void	ft_putnbr_rec(unsigned int n, int *fd, int mult)
 {
 	char			c;
 	unsigned int	tmp;
@@ -23,14 +23,14 @@ static void	ft_putnbr_rec(unsigned int n, int fd, int mult)
 		write(fd, &c, 1);
 		tmp = n / mult;
 		tmp *= mult;
-		ft_putnbr_rec((n - tmp), fd, mult / 10);
+		ft_putnbr_rec((n - tmp), *fd, mult / 10);
 	}
 	return ;
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int				mult;
+	long			mult;
 	unsigned int	nbr;
 
 	mult = 1;
@@ -43,47 +43,5 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	else
 		nbr = n;
-	ft_putnbr_rec(nbr, fd, mult);
+	ft_putnbr_rec(nbr, &fd, mult);
 }
-
-/*static void	ft_putnbr_rec(unsigned int n, int fd)
-{
-	unsigned int	ncpy;
-	char			c;
-
-	ncpy = n;
-	while (n / 10 != 0)
-		n /= 10;
-	c = '0' + n;
-	write(fd, &c, 1);
-	if (n != 0)
-		ft_putnbr_rec(ncpy / 10, fd);
-}
-
-void	ft_putnbr_fd(int n, int fd)
-{
-	unsigned int	nbr;
-
-	if (n < 0)
-	{
-		nbr = n * -1;
-		write(fd, "-", 1);
-	}
-	else
-		nbr = n;
-	ft_putnbr_rec(nbr, fd);
-}*/
-
-/*
-void	ft_putnbr_fd(int n, int fd)
-{
-	char	*nbr;
-
-	nbr = ft_itoa(n);
-	if (nbr)
-	{
-		ft_putstr_fd(nbr, fd);
-		free(nbr);
-	}
-}
-*/
