f75aebbb32428ef7eab285e5a1ad19db82d91ffd
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-22 13:19:29 -0300

    New algorithm

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,12 +6,68 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:14:39 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/20 08:52:51 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/22 13:18:56 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+**	Function name:
+**		> ft_count_digits
+**	Parameters:
+**		> n (int) : Number from which the digits will be counted
+**	Description:
+**		> Count how many digits the parameter n has, then return that amount
+**	Return:
+**		> (int). Return the amount of digits in number n
+*/
+
+static int	ft_count_digits(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len = 1;
+	while (n != 0)
+	{
+		len += 1;
+		n /= 10;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int				len;
+	unsigned int	nbr;
+	char			*str;
+
+	len = ft_count_digits(nbr);
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	str[len--] = '\0';
+	if (nbr == 0)
+		str[0] = '0';
+	else if (n < 0)
+	{
+		str[0] = '-';
+		nbr = n * (-1);
+	}
+	else
+		nbr = n;
+	while (nbr > 0 || len >= 0)
+	{
+		str[len] = (nbr % 10) + '0';
+		nbr /= 10;
+		len -= 1;
+	}
+	return (str);
+}
+
+/*
 static int	ft_get_signal(int n)
 {
 	if (n < 0)
@@ -65,3 +121,4 @@ char	*ft_itoa(int n)
 	*(str + len) = '\0';
 	return (str);
 }
+*/
