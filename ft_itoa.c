91f3c36a127c5dff09fac8330638f18fd92f47c7
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-22 13:44:29 -0300

    Removed comments

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:14:39 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/22 13:39:37 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/22 13:44:19 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ char	*ft_itoa(int n)
 	str = ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
+	str[len--] = '\0';
 	if (n == 0)
 		str[0] = '0';
 	else if (n < 0)
@@ -59,65 +60,9 @@ char	*ft_itoa(int n)
 		nbr = n;
 	while (nbr > 0)
 	{
-		len -= 1;
 		str[len] = (nbr % 10) + '0';
 		nbr /= 10;
+		len -= 1;
 	}
 	return (str);
 }
-
-/*
-static int	ft_get_signal(int n)
-{
-	if (n < 0)
-		return (-1);
-	return (1);
-}
-
-static int	ft_get_len(int n)
-{
-	int	len;
-
-	if (n == 0)
-		return (1);
-	len = 0;
-	if (ft_get_signal(n) < 0)
-		len += 1;
-	while (n != 0)
-	{
-		len += 1;
-		n /= 10;
-	}
-	return (len);
-}
-
-char	*ft_itoa(int n)
-{
-	int		len;
-	int		signal;
-	int		i;
-	char	*str;
-	char	first_nbr;
-
-	len = ft_get_len(n);
-	signal = ft_get_signal(n);
-	str = ft_calloc(len + 1, sizeof(char));
-	if (!str)
-		return (NULL);
-	first_nbr = 0;
-	if (signal < 0)
-	{
-		*str = '-';
-		first_nbr = 1;
-	}
-	i = len - 1;
-	while (i >= first_nbr)
-	{
-		*(str + i) = ((n % 10) * signal) + '0';
-		n /= 10;
-		i -= 1;
-	}
-	*(str + len) = '\0';
-	return (str);
-}
-*/
