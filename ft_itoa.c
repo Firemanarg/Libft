41fcf4a58faeb6994f968ee3e2e0ffc77eced550
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-22 13:40:06 -0300

    Fixed wrong variable at condition

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:14:39 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/22 13:28:08 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/22 13:39:37 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,7 @@ char	*ft_itoa(int n)
 	str = ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	str[len--] = '\0';
-	if (nbr == 0)
+	if (n == 0)
 		str[0] = '0';
 	else if (n < 0)
 	{
@@ -60,9 +59,9 @@ char	*ft_itoa(int n)
 		nbr = n;
 	while (nbr > 0)
 	{
+		len -= 1;
 		str[len] = (nbr % 10) + '0';
 		nbr /= 10;
-		len -= 1;
 	}
 	return (str);
 }
