87f036786b98a155bffd6c685a7b997f52578b74
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-24 12:02:30 -0300

    Reimplemented logic

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:52 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/23 15:59:29 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/24 12:02:11 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	str_quantity(char const *str, char del);
+/*static size_t	str_quantity(char const *str, char del);
 static void		complement_split(char const *s, char c, char **arr);
 
 char	**ft_split(char const *s, char c)
@@ -73,52 +73,63 @@ static size_t	word_count(char const *s, char c)
 			i += 1;
 	}
 	return (count);
+}*/
+
+static void	freeall(char **array, size_t curr_index)
+{
+	while (curr_index > 0)
+	{
+		free(*(array + curr_index));
+		curr_index -= 1;
+	}
+	free(array[0]);
+	free(array);
 }
 
 static char	**fill_array(char const *s, char c, char **array)
 {
 	size_t	i;
-	size_t	cchr;
-	size_t	carr;
+	size_t	found_chr;
+	size_t	curr_str;
 
 	i = 0;
-	carr = 0;
+	curr_str = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-			i++;
-		else
+		if (s[i] != c)
 		{
-			cchr = 0;
+			found_chr = i;
 			while (s[i] != c && s[i] != '\0')
+				i += 1;
+			array[curr_str] = ft_substr(s, found_chr, (i - found_chr));
+			if (array[curr_str] == NULL)
 			{
-				i++;
-				cchr++;
+				freeall(array, i);
+				return (NULL);
 			}
-			arr[carr] = ft_calloc(cchr + 1, sizeof(char));
-			ft_strlcpy(arr[carr], &s[i - cchr], cchr + 1);
-			carr += 1;
+			curr_str += 1;
 		}
+		else
+			i += 1;
 	}
-	// Pendente -> Finalizar implementaçao desta função;
-	//					Preencher o array com os valores. Utilizar substr pra
-	//					obter os valores
-	while (s[i] != '\0')
-	{
-
-	}
+	return (array);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**array;
+	size_t	wc;
 
 	if (!s)
 		return (NULL);
-	array = ft_calloc(word_count(s, c) + 1, sizeof(char *));
+	wc = word_count(s, c);
+	array = ft_calloc(wc + 1, sizeof(char *));
 	if (!array)
 		return (NULL);
-
+	array = fill_array(s, c, array);
+	if (array)
+		array[wc] = NULL;
+	return (array);
 }
 
 /*
