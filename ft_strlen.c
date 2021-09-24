6914cee19f7565d2b53a12cb3f7a4464c9c0b787
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2021-09-24 13:47:10 -0300

    Optimizations

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 11:13:28 by lsilva-q          #+#    #+#             */
-/*   Updated: 2021/09/24 13:45:16 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2021/09/24 13:47:03 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 size_t	ft_strlen(const char *str)
 {
-	size_t	len;
 	char	*s;
 
-	len = 0;
 	s = (char *) str;
-	while (s)
+	while (*s)
 		s++;
 	return (s - str);
 }
